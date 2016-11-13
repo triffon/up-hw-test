d533c3770c78a1d0c732d26b9cb2e32c7ead1c18
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2016-11-13 11:25:16 +0200

    Създаване на нова results директория при всяко пускане на тестовете

--- run_tests.sh
+++ run_tests.sh
@@ -3,8 +3,18 @@
 BASE=/usr/src/up-hw-test
 
 cd "$1"
+
+# create a fresh results dir each time
+TIMESTAMP=`date +%Y%m%d%H%M%S`
+RESULTS_DIR=results-$TIMESTAMP
+
+
+mkdir $RESULTS_DIR
 docker run --rm \
-       -v `pwd`/results:$BASE/results \
+       -v `pwd`/$RESULTS_DIR:$BASE/results \
        -v `pwd`/progs:$BASE/progs:ro \
        -v `pwd`/tests:$BASE/tests:ro \
        up-hw-test
+
+# archiving results
+zip $RESULTS_DIR.zip $RESULTS_DIR
