3a3fbe4d63c62b485d42b5b633101d920a19b6ae
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2016-11-13 11:24:41 +0200

    Добавена възможност за построяване на Docker образ за конкретен набор от тестове

--- build_docker.sh
+++ build_docker.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-docker build -t up-hw-test .
+
+BASE_IMAGE=up-hw-test
+
+if [ $# -gt 0 -a -d "$1" ]; then
+    # build an image for a specific set of tests
+    DIR="$1"
+    DIR_NAME=`basename "$DIR"`
+    IMAGE_NAME=$BASE_IMAGE-$DIR_NAME
+    echo "Building image $IMAGE_NAME"
+
+    # copy Dockerfile to build context
+    cp Dockerfile.tests "$DIR"/Dockerfile
+
+    docker build -t $IMAGE_NAME "$DIR"
+else
+    # build the generic image in which tests are attached as a volume
+    echo "Building image $BASE_IMAGE"
+    docker build -t $BASE_IMAGE .
+fi
