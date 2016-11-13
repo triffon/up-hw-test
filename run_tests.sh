6cd129386040f7746546c5ba215d3a07880ce079
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2016-11-13 11:43:01 +0200

    Промених скрипта за пускане на тестове да може да пуска специфичния Docker образ за съответното домашно, ако е създаден такъв

--- run_tests.sh
+++ run_tests.sh
@@ -1,20 +1,51 @@
 #!/bin/sh
 
 BASE=/usr/src/up-hw-test
+BASE_IMAGE=up-hw-test
 
-cd "$1"
+if [ $# -lt 1 ]; then
+    echo "Usage: run_tests.sh <directory_to_test>"
+    exit 1
+fi
+
+DIR="$1"
+
+cd "$DIR"
+
+DIR_NAME=`basename "$DIR"`
+DIR_IMAGE=$BASE_IMAGE-$DIR_NAME
+
+# which image to run? The generic one or the specific one?
+if docker images | grep $DIR_IMAGE > /dev/null 2> /dev/null; then
+    # there is a specific image run it
+    IMAGE=$DIR_IMAGE
+    OPTS=
+else
+    # no specific image, run the general one
+    # and hope there are tests
+    IMAGE=$BASE_IMAGE
+    OPTS="-v `pwd`/tests:$BASE/tests:ro"
+fi
 
 # create a fresh results dir each time
 TIMESTAMP=`date +%Y%m%d%H%M%S`
 RESULTS_DIR=results-$TIMESTAMP
-
-
 mkdir $RESULTS_DIR
+
+# run the image
+
+echo "Running image $IMAGE..."
 docker run --rm \
        -v `pwd`/$RESULTS_DIR:$BASE/results \
        -v `pwd`/progs:$BASE/progs:ro \
-       -v `pwd`/tests:$BASE/tests:ro \
-       up-hw-test
+       $OPTS \
+       $IMAGE
+
+echo "Done, results written to $RESULTS_DIR"
 
 # archiving results
-zip $RESULTS_DIR.zip $RESULTS_DIR
+ARCHIVE="$RESULTS_DIR.zip"
+
+echo "Archiving results to $ARCHIVE..."
+zip -q "$ARCHIVE" "$RESULTS_DIR" > /dev/null
+echo "Done"
